ab6d71eac79ee90fd74ed504b984c9be2609a0b3
Philipp Schweig <philippschweig@users.noreply.github.com>
2017-07-23 09:31:11 +0200

    ViewModel for NetworkConnectionRule

--- OpenNetworkConnections/View/PluginView.xaml.cs
+++ OpenNetworkConnections/View/PluginView.xaml.cs
@@ -24,24 +24,38 @@ namespace de.efsdev.wsapm.OpenNetworkConnections.View
     /// </summary>
     public partial class PluginView : UserControl, IWsapmPluginSettingsControl
     {
+        private PluginViewModel ViewModel => ViewModelLocator.PluginViewModel;
+
         public PluginView()
         {
             InitializeComponent();
-            this.DataContext = ViewModelLocator.PluginViewModel;
+            ViewModel.OnActiveConnectionsRefreshed += ViewModel_OnActiveConnectionsRefreshed;
+            this.UpdateDataContext();
+        }
+
+        private void ViewModel_OnActiveConnectionsRefreshed(object sender, EventArgs e)
+        {
+            this.UpdateDataContext();
+        }
+
+        private void UpdateDataContext()
+        {
+            this.DataContext = null;
+            this.DataContext = ViewModel;
         }
 
         public object GetSettingsBeforeSave()
         {
             // Build up a new instance of your settings class here 
             // and fill it with the elements from your UI.
-            return ViewModelLocator.PluginViewModel.Settings;
+            return ViewModel.Settings;
         }
 
         public void SetSettings(object settings)
         {
             // In this method, the current settings of your plugin gets injected.
             // Use this method to initialize the UI, i.e. filling the UI elements from the current settings.
-            ViewModelLocator.PluginViewModel.SetSettingsFromObject(settings);
+            ViewModel.SetSettingsFromObject(settings);
         }
     }
 }
